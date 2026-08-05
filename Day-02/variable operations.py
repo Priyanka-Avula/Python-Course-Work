ad5a0760d47fdefc14b2856b8928fa65d3c10486
Monika Sai Priyanka Avula <arangarao411@gmail.com>
2026-08-05 17:46:48 +0530

    Python Intro

---
+++ Day-02/variable operations.py
@@ -0,0 +1,38 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================================== RESTART: C:/Users/monik/OneDrive/Desktop/Python-Course-work/Day-02/Keywords.py ====================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+a=10
+b=20
+a
+10
+b
+20
+a=b=c=10
+a
+10
+b
+10
+c
+10
+a,b,c=10,20,30
+a
+10
+b
+20
+c
+30
+a,b=b,a
+a
+20
+del a
+a
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    a
+NameError: name 'a' is not defined
+b=20
+b
+20
